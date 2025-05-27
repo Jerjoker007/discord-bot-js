fa32206f92ca513949ebcf66aa8336cfd5d284d5
Jerjoker007 <jerjoker.paq@gmail.com>
2025-05-27 14:05:02 -0400

    Added files with info on Ravi's bounty rewards

--- dist/events/interactionCreate/handleButtons.js
+++ dist/events/interactionCreate/handleButtons.js
@@ -13,47 +13,52 @@ module.exports = async (client, interaction) => {
 
     const submitChannel = '1374042127121518785';
     const { botName, action, params} = decodeCustomId(interaction.customId);
-
+    
     if (botName === 'ravi') {
-        if (action === 'confirm') {
 
-            if (params.userId === interaction.member.id) {
-                interaction.reply({
-                    content: 'You cannot authorize your own submission.',
-                    flags: MessageFlags.Ephemeral,
-                });
-            } else {
+        try {
+            if (action === 'confirm') {
+    
+                if (params.userId === interaction.member.id) {
+                    interaction.reply({
+                        content: 'You cannot authorize your own submission.',
+                        flags: MessageFlags.Ephemeral,
+                    });
+                } else {
+                    const components = interaction.message.components.map(row => {
+                        return new ActionRowBuilder().addComponents(
+                            row.components.map(btn => ButtonBuilder.from(btn).setDisabled(true))
+                        );
+                    });
+    
+                    await interaction.update({ 
+                        content: '✅ This submission has been accepted.',
+                        components 
+                    });
+    
+                    await client.channels.cache.get(submitChannel).send(`<@${params.userId}> Your submission was accepted by ${interaction.member.user.username} and the rewards has been sent.`);
+    
+                    await submissionTracker.unmarkSubmitted(params.userId);
+                }
+            } else if (action === 'reject') {
+    
                 const components = interaction.message.components.map(row => {
                     return new ActionRowBuilder().addComponents(
                         row.components.map(btn => ButtonBuilder.from(btn).setDisabled(true))
                     );
                 });
-
+    
                 await interaction.update({ 
-                    content: '✅ This submission has been accepted.',
+                    content: '❌ This submission has been rejected.',
                     components 
                 });
-
-                await client.channels.cache.get(submitChannel).send(`<@${params.userId}> Your submission was accepted by ${interaction.member.user.username} and the rewards has been sent.`);
-
+    
+                await client.channels.cache.get(submitChannel).send(`<@${params.userId}> Your submission was rejected by ${interaction.member.user.username}.`);
+    
                 await submissionTracker.unmarkSubmitted(params.userId);
             }
-        } else if (action === 'reject') {
-
-            const components = interaction.message.components.map(row => {
-                return new ActionRowBuilder().addComponents(
-                    row.components.map(btn => ButtonBuilder.from(btn).setDisabled(true))
-                );
-            });
-
-            await interaction.update({ 
-                content: '❌ This submission has been rejected.',
-                components 
-            });
-
-            await client.channels.cache.get(submitChannel).send(`<@${params.userId}> Your submission was rejected by ${interaction.member.user.username}.`);
-
-            await submissionTracker.unmarkSubmitted(params.userId);
+        } catch (error) {
+            console.error('There was an error with the button interaction.', error);        
         }
     }
 };
