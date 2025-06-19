74b204f22eec1cee308f8453178e878ae7aad78b
Jerjoker007 <jerjoker.paq@gmail.com>
2025-06-19 11:27:49 -0400

    Register Command now go through all guilds

--- src/events/ready/01registerCommands.js
+++ src/events/ready/01registerCommands.js
@@ -6,35 +6,37 @@ const areCommandsDifferent = require('../../utils/areCommandsDifferent');
 module.exports = async (client) => {
     try {
         const localCommands = getLocalCommands();
-        const applicationCommands = await getApplicationCommands(client, testServer);
-        for (const localCommand of localCommands) {
-            const { name, description, options } = localCommand;
-            const existingCommand = await applicationCommands.cache.find((cmd) => cmd.name === name);
-            if (existingCommand) {
-                if (localCommand.deleted) {
-                    await applicationCommands.delete(existingCommand.id);
-                    console.log(`Deleted command "${name}".`);
-                    continue;
+        for (const [guildId] of client.guilds.cache ) {
+            const applicationCommands = await getApplicationCommands(client, guildId);
+            for (const localCommand of localCommands) {
+                const { name, description, options } = localCommand;
+                const existingCommand = await applicationCommands.cache.find((cmd) => cmd.name === name);
+                if (existingCommand) {
+                    if (localCommand.deleted) {
+                        await applicationCommands.delete(existingCommand.id);
+                        console.log(`Deleted command "${name}".`);
+                        continue;
+                    }
+                    if (areCommandsDifferent(existingCommand, localCommand)) {
+                        await applicationCommands.edit(existingCommand.id, {
+                            description,
+                            options,
+                        });
+                        console.log(`Edited command "${name}".`);
+                    }
                 }
-                if (areCommandsDifferent(existingCommand, localCommand)) {
-                    await applicationCommands.edit(existingCommand.id, {
+                else {
+                    if (localCommand.deleted) {
+                        console.log(`Skipping registering command "${name}" as it's set to delete.`);
+                        continue;
+                    }
+                    await applicationCommands.create({
+                        name,
                         description,
                         options,
                     });
-                    console.log(`Edited command "${name}".`);
+                    console.log(`Registered command"${name}".`);
                 }
-            }
-            else {
-                if (localCommand.deleted) {
-                    console.log(`Skipping registering command "${name}" as it's set to delete.`);
-                    continue;
-                }
-                await applicationCommands.create({
-                    name,
-                    description,
-                    options,
-                });
-                console.log(`Registered command"${name}".`);
             }
         }
     }
